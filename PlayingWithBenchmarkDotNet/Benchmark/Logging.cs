0dab7d17686a503342f7b78efe731efe0fe40441
Balázs Keresztúri <19balazs86@gmail.com>
2019-07-12 19:26:08 +0100

    New example: Logging.

---
+++ PlayingWithBenchmarkDotNet/Benchmark/Logging.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace PlayingWithBenchmarkDotNet.Benchmark
+{
+  [RankColumn]
+  [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+  [MemoryDiagnoser]
+  public class Logging
+  {
+    private readonly ILogger _logger = NullLogger.Instance;
+
+    private readonly Action<ILogger, string, int, Exception> _definedLogger
+      = LoggerMessage.Define<string, int>(
+          logLevel: LogLevel.Information,
+          eventId: 1,
+          formatString: "Name: '{n}', Age: {a}");
+
+    [Benchmark(Baseline = true)]
+    public void LogStringInterpolation() => _logger.LogInformation($"Name: '{"Name"}', Age: {0}");
+
+    [Benchmark]
+    public void LogWithMessageAndParams() => _logger.LogInformation("Name: '{n}', Age: {a}", "Name", 0);
+
+    [Benchmark]
+    public void LogWithPredefined() => _definedLogger(_logger, "Name", 0, null);
+  }
+}
--- PlayingWithBenchmarkDotNet/Program.cs
+++ PlayingWithBenchmarkDotNet/Program.cs
@@ -7,8 +7,9 @@ namespace PlayingWithBenchmarkDotNet
   {
     public static void Main(string[] args)
     {
-      //var summary = BenchmarkRunner.Run<BenchmarkMd5VsSha256>();
-      var summary = BenchmarkRunner.Run<NameParsers>();
+      //var summary = BenchmarkRunner.Run<Md5VsSha256>();
+      //var summary = BenchmarkRunner.Run<NameParsers>();
+      var summary = BenchmarkRunner.Run<Logging>();
     }
   }
 }
