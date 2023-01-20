833593561140363269fb4a24a2d73d7c0978f6db
Balazs Kereszturi <19balazs86@gmail.com>
2023-01-20 17:21:15 +0100

    Add Base64UrlEncoder. But needs to work on it.

---
+++ PlayingWithBenchmarkDotNet/Code/Base64UrlEncoder.cs
@@ -0,0 +1,85 @@
+﻿using System.Buffers.Text;
+using System.Text;
+
+namespace PlayingWithBenchmarkDotNet.Code;
+
+// TODO: Needs to work on it, something is odd...
+public static class Base64UrlEncoder
+{
+    private const char _charEquals     = '=';
+    private const char _charSlash      = '/';
+    private const char _charHyphen     = '-';
+    private const char _charPlus       = '+';
+    private const char _charUnderscore = '_';
+    private const byte _byteEquals     = (byte)'=';
+    private const byte _byteSlash      = (byte)'/';
+    private const byte _bytePlus       = (byte)'+';
+
+    public static string Encode(ReadOnlySpan<char> input)
+    {
+        Span<byte> inputAsUTF8Bytes = stackalloc byte[Encoding.UTF8.GetByteCount(input)];
+
+        Encoding.UTF8.GetBytes(input, inputAsUTF8Bytes);
+
+        int base64EncodedLength = Base64.GetMaxEncodedToUtf8Length(inputAsUTF8Bytes.Length);
+
+        Span<byte> base64Bytes = stackalloc byte[base64EncodedLength];
+
+        Base64.EncodeToUtf8(inputAsUTF8Bytes, base64Bytes, out _, out _);
+
+        int paddingSize = base64Bytes switch
+        {
+            [.., _byteEquals, _byteEquals] => 2,
+            [.., _byteEquals]              => 1,
+            _                              => 0
+        };
+
+        Span<char> finalChars = stackalloc char[base64EncodedLength - paddingSize];
+
+        for (int i = 0; i < finalChars.Length; i++)
+        {
+            finalChars[i] = base64Bytes[i] switch
+            {
+                _byteSlash => _charHyphen,
+                _bytePlus  => _charUnderscore,
+                _          => (char)base64Bytes[i]
+            };
+        }
+
+        return new string(finalChars);
+    }
+
+    public static string Decode(ReadOnlySpan<char> base64Input)
+    {
+        int paddingSize = (base64Input.Length % 4) switch
+        {
+            0     => 0,
+            int n => 4 - n
+        };
+
+        Span<byte> base64bytes = stackalloc byte[base64Input.Length + paddingSize];
+
+        for (int i = 0; i < base64Input.Length; i++)
+        {
+            base64bytes[i] = base64Input[i] switch
+            {
+                _charHyphen     => _byteSlash,
+                _charUnderscore => _bytePlus,
+                _               => (byte)base64Input[i]
+            };
+        }
+
+        for (int i = paddingSize; i > 0; i--)
+            base64bytes[^i] = _byteEquals;
+
+        Span<byte> utf8Bytes = stackalloc byte[Base64.GetMaxDecodedFromUtf8Length(base64bytes.Length)];
+
+        Base64.DecodeFromUtf8(base64bytes, utf8Bytes, out int consumed, out int bytesWritten);
+
+        Span<char> finalChars = stackalloc char[bytesWritten];
+
+        Encoding.UTF8.GetChars(utf8Bytes[..bytesWritten], finalChars);
+
+        return finalChars.ToString();
+    }
+}
--- PlayingWithBenchmarkDotNet/Code/Guider.cs
+++ PlayingWithBenchmarkDotNet/Code/Guider.cs
@@ -1,84 +1,84 @@
 ﻿using System.Buffers.Text;
 using System.Runtime.InteropServices;
 
-namespace PlayingWithBenchmarkDotNet.Code
+namespace PlayingWithBenchmarkDotNet.Code;
+
+// YouTube - Nick: https://youtu.be/B2yOjLyEZk0
+// https://www.stevejgordon.co.uk/using-high-performance-dotnetcore-csharp-techniques-to-base64-encode-a-guid
+public static class Guider
 {
-    // YouTube - Nick: https://youtu.be/B2yOjLyEZk0
-    // https://www.stevejgordon.co.uk/using-high-performance-dotnetcore-csharp-techniques-to-base64-encode-a-guid
-    public static class Guider
+    private const char _charEquals     = '=';
+    private const char _charSlash      = '/';
+    private const char _charHyphen     = '-';
+    private const char _charPlus       = '+';
+    private const char _charUnderscore = '_';
+    private const byte _byteEquals     = (byte)'=';
+    private const byte _byteSlash      = (byte)'/';
+    private const byte _bytePlus       = (byte)'+';
+
+    public static string ToStringFromGuid(Guid id)
     {
-        private const char _charEquals     = '=';
-        private const char _charSlash      = '/';
-        private const char _charHyphen     = '-';
-        private const char _charPlus       = '+';
-        private const char _charUnderscore = '_';
-        private const byte _byteEquals     = (byte)'=';
-        private const byte _byteSlash      = (byte)'/';
-        private const byte _bytePlus       = (byte)'+';
+        return Convert.ToBase64String(id.ToByteArray())
+            .Replace("/", "-")
+            .Replace("+", "_")
+            .Replace("=", string.Empty);
+    }
 
-        public static string ToStringFromGuid(Guid id)
+    public static Guid ToGuidFromString(string id)
+    {
+        id = id.Replace("-", "/").Replace("_", "+") + "==";
+
+        byte[] base64StrArray = Convert.FromBase64String(id);
+
+        return new Guid(base64StrArray);
+    }
+
+    public static string ToStringFromGuidOp(Guid id)
+    {
+        Span<byte> idBytes     = stackalloc byte[16];
+        Span<byte> base64Bytes = stackalloc byte[24]; // With padding -> 4 * Math.Ceiling(X / 3.0) where X = 16
+
+        MemoryMarshal.TryWrite(idBytes, ref id);
+
+        Base64.EncodeToUtf8(idBytes, base64Bytes, out _, out _);
+
+        Span<char> finalChars = stackalloc char[22]; // Without padding -> Math.Ceiling(X * 8 / 6.0) where X = 16
+
+        for (int i = 0; i < 22; i++)
         {
-            return Convert.ToBase64String(id.ToByteArray())
-                .Replace("/", "-")
-                .Replace("+", "_")
-                .Replace("=", string.Empty);
-        }
-
-        public static Guid ToGuidFromString(string id)
-        {
-            id = id.Replace("-", "/").Replace("_", "+") + "==";
-
-            byte[] base64StrArray = Convert.FromBase64String(id);
-
-            return new Guid(base64StrArray);
-        }
-
-        public static string ToStringFromGuidOp(Guid id)
-        {
-            Span<byte> idBytes     = stackalloc byte[16];
-            Span<byte> base64Bytes = stackalloc byte[24];
-
-            MemoryMarshal.TryWrite(idBytes, ref id);
-
-            Base64.EncodeToUtf8(idBytes, base64Bytes, out _, out _);
-
-            Span<char> finalChars = stackalloc char[22];
-
-            for (int i = 0; i < 22; i++)
+            finalChars[i] = base64Bytes[i] switch
             {
-                finalChars[i] = base64Bytes[i] switch
-                {
-                    _byteSlash => _charHyphen,
-                    _bytePlus => _charUnderscore,
-                    _ => (char)base64Bytes[i]
-                };
-            }
-
-            return new string(finalChars);
+                _byteSlash => _charHyphen,
+                _bytePlus => _charUnderscore,
+                _ => (char)base64Bytes[i]
+            };
         }
 
-        public static Guid ToGuidFromStringOp(ReadOnlySpan<char> id)
+        return new string(finalChars);
+    }
+
+    public static Guid ToGuidFromStringOp(ReadOnlySpan<char> id)
+    {
+        Span<char> base64Chars = stackalloc char[24];
+
+        for (int i = 0; i < 22; i++)
         {
-            Span<char> base64Chars = stackalloc char[24];
-
-            for (int i = 0; i < 22; i++)
+            base64Chars[i] = id[i] switch
             {
-                base64Chars[i] = id[i] switch
-                {
-                    _charHyphen     => _charSlash,
-                    _charUnderscore => _charPlus,
-                    _               => id[i]
-                };
-            }
-
-            base64Chars[22] = _charEquals;
-            base64Chars[23] = _charEquals;
-
-            Span<byte> idBytes = stackalloc byte[16];
-
-            Convert.TryFromBase64Chars(base64Chars, idBytes, out _);
-
-            return new Guid(idBytes);
+                _charHyphen     => _charSlash,
+                _charUnderscore => _charPlus,
+                _               => id[i]
+            };
         }
+
+        base64Chars[22] = _charEquals;
+        base64Chars[23] = _charEquals;
+
+        // (3 * (length_in_chars / 4)) - number_of_padding_chars
+        Span<byte> idBytes = stackalloc byte[16];
+
+        Convert.TryFromBase64Chars(base64Chars, idBytes, out _);
+
+        return new Guid(idBytes);
     }
 }
