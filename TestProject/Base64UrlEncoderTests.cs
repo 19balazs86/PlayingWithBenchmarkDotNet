2227c70f2cecfd7fc98fc32c954406632c2f71de
Balazs Kereszturi <19balazs86@gmail.com>
2023-03-18 08:53:29 +0100

    Small refactor: using ClassData instead of MemberData

--- TestProject/Base64UrlEncoderTests.cs
+++ TestProject/Base64UrlEncoderTests.cs
@@ -6,7 +6,7 @@ namespace TestProject;
 public class Base64UrlEncoderTests
 {
     [Theory]
-    [MemberData(nameof(GetData))]
+    [ClassData(typeof(EncodingData))]
     public void Encode_Decode_Simple(string input)
     {
         // Arrange
@@ -26,7 +26,7 @@ public class Base64UrlEncoderTests
     }
 
     [Theory]
-    [MemberData(nameof(GetData))]
+    [ClassData(typeof(EncodingData))]
     public void Encode_Decode_Complex(string input)
     {
         // Arrange
@@ -47,7 +47,7 @@ public class Base64UrlEncoderTests
     }
 
     [Theory]
-    [MemberData(nameof(GetData))]
+    [ClassData(typeof(EncodingData))]
     public void Encode_Decode_UrlFriendly_Simplest(string input)
     {
         // Act
@@ -60,7 +60,7 @@ public class Base64UrlEncoderTests
     }
 
     [Theory]
-    [MemberData(nameof(GetData))]
+    [ClassData(typeof(EncodingData))]
     public void Encode_Decode_UrlFriendly_Simple(string input)
     {
         // Act
@@ -73,7 +73,7 @@ public class Base64UrlEncoderTests
     }
 
     [Theory]
-    [MemberData(nameof(GetData))]
+    [ClassData(typeof(EncodingData))]
     public void Encode_Decode_UrlFriendly_Complex(string input)
     {
         // Act
@@ -84,30 +84,31 @@ public class Base64UrlEncoderTests
         // Assert
         Assert.Equal(input, decodedText);
     }
-
-    public static IEnumerable<object[]> GetData()
-    {
-        return new List<object[]>
-        {
-            new object[] { "=" },
-            new object[] { "==" },
-            new object[] { "===" },
-            new object[] { "====" },
-            new object[] { "" },
-            new object[] { "1" },
-            new object[] { "12" },
-            new object[] { "123" },
-            new object[] { "1234" },
-            new object[] { "12345" },
-            new object[] { "123456" },
-            new object[] { "1234567" },
-            new object[] { "12345678" },
-            new object[] { "123456789" },
-            new object[] { "1234567890" },
-            new object[] { "\"\\~ˇ^˘°˛`˙´˝¨¸×÷¤ß$Łł*>;}{@&#><" },
-            new object[] { "§'+!%/=()?:_" },
-            new object[] { "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" },
-            new object[] { "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" }
-        };
-    }
 }
+
+// https://www.milanjovanovic.tech/blog/creating-data-driven-tests-with-xunit
+file sealed class EncodingData : TheoryData<string>
+{
+    public EncodingData()
+    {
+        Add("=");
+        Add("==");
+        Add("===");
+        Add("====");
+        Add("");
+        Add("1");
+        Add("12");
+        Add("123");
+        Add("1234");
+        Add("12345");
+        Add("123456");
+        Add("1234567");
+        Add("12345678");
+        Add("123456789");
+        Add("1234567890");
+        Add("\"\\~ˇ^˘°˛`˙´˝¨¸×÷¤ß$Łł*>;}{@&#><");
+        Add("§'+!%/=()?:_");
+        Add("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+        Add("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+    }
+};
