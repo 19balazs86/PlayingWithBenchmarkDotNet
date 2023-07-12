fd28833c57f9088521123249990324a904b4268a
Balazs Kereszturi <19balazs86@gmail.com>
2023-07-12 08:20:49 +0200

    Remove old benchmarks

--- PlayingWithBenchmarkDotNet/Program.cs
+++ PlayingWithBenchmarkDotNet/Program.cs
@@ -1,10 +1,7 @@
 ﻿using BenchmarkDotNet.Running;
 using PlayingWithBenchmarkDotNet.Benchmark;
 
-//_ = BenchmarkRunner.Run<Md5VsSha256_Benchmarks>();
-//_ = BenchmarkRunner.Run<NameParser_Benchmarks>();
 //_ = BenchmarkRunner.Run<Logging_Benchmarks>();
-//_ = BenchmarkRunner.Run<ReadJsonProperty_Benchmarks>();
 //_ = BenchmarkRunner.Run<Guider_Benchmarks>();
 //_ = BenchmarkRunner.Run<BasicAuthentication_Benchmark>();
 //_ = BenchmarkRunner.Run<Base64Encoder_Benchmark>();
