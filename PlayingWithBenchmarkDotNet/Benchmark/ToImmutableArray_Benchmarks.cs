f3e97c7475dbf14f0adc86c117b792df08b7041b
Balazs Kereszturi <19balazs86@gmail.com>
2023-07-13 17:29:11 +0200

    Add benchmark result table

--- PlayingWithBenchmarkDotNet/Benchmark/ToImmutableArray_Benchmarks.cs
+++ PlayingWithBenchmarkDotNet/Benchmark/ToImmutableArray_Benchmarks.cs
@@ -7,6 +7,13 @@ namespace PlayingWithBenchmarkDotNet.Benchmark;
 // Solving 1 of the Biggest Array issues
 // Nick Chapsas: https://youtu.be/lT5n2o3kuno
 
+/*
+|                       Method | ItemCount |        Mean |     Error |    StdDev |   Gen0 | Allocated |
+|----------------------------- |---------- |------------:|----------:|----------:|-------:|----------:|
+|                       Normal |       100 | 112.5971 ns | 1.0058 ns | 0.8917 ns | 0.1969 |     824 B |
+| With_UnsafeAs_ImmutableArray |       100 |   0.0000 ns | 0.0000 ns | 0.0000 ns |      - |         - |
+*/
+
 [MemoryDiagnoser]
 public class ToImmutableArray_Benchmarks
 {
